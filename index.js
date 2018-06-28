3dd6dba6fe1b1fc35b2001c66615d3eb12b19545
Sam Ashtar <samashtarm@gmail.com>
2018-06-28 22:54:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = "I don't do much.";
 
 var kittens = ["Milo", "Otis", "Garfield"]
-function destructivelyAppendKitten("Ralph") {
-  
+function destructivelyAppendKitten ("Ralph") {
+  return kittens.push("Ralph")
 }
