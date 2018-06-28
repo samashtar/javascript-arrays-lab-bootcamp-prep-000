08e8fedacf7e3360ea92c99d62c8c59f996e007d
Sam Ashtar <samashtarm@gmail.com>
2018-06-28 23:17:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,8 +28,7 @@ function appendKitten(name) {
 
 }
 function prependKitten(name) {
-  [name, ...kittens]
-  return kittens
+  return [name, ...kittens]
 }
 function removeLastKitten() {
 kittens.slice(0, kittens.length-1)
